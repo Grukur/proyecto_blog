1515215beb25369b299c24cafd0d028c1db4a42b
Grukur <darold.trench@gmail.com>
2023-07-18 16:42:31 -0400

    update

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -19,7 +19,7 @@ export const emitToken = async (req, res, next) => {
             exp: exp,
             data: usuario,
         },
-        process.env.production.PASSWORD_SECRET
+        process.env.PASSWORD_SECRET
     );
     const expTimestamp = exp;    
 
